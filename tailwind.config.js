27c3c5caddfe7b34d8342f618ad55fe8141e1d54
SOUHAIL <souhailmagrane@gmail.com>
2024-06-09 13:52:55 +0200

    NIKE
    add files left in nike projrct

---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)"
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      backgroundImage: {
+        'hero': "url('assets/images/collection-background.svg')",
+        'card': "url('assets/images/thumbnail-background.svg')",
+      },
+      screens: {
+        'x3' : '300px',
+        'x4':  '400px',
+        'x5':  '500px',
+        'x6':  '600px',
+        'm7':  '700px',
+        'm8':  '800px',
+        'm9':  '900px',
+        'l10': '1000px',
+        'l11' : '1100px',
+        'l12' : '1200px',
+        'l13' : '1300px',
+        "wide": "1400px",
+        'full' : '1500px'
+      },
+      filter : {
+        'filter-btn' : ''
+      }
+    },
+  },
+  plugins: [],
+}
